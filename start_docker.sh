b6013c79cee01f7bdcf78541d7aefd684d5eeea1
Alejandro Queiruga <afqueiruga@lbl.gov>
2017-11-27 15:35:41 -0800

    Another notebook ; environment loading scripts

---
+++ start_docker.sh
@@ -0,0 +1,1 @@
+docker run -it -v `pwd`:/notebooks gcr.io/tensorflow/tensorflow bash
---
+++ start_venv.sh
@@ -0,0 +1,2 @@
+source ~/opt/tensorflow/bin/activate
+alias ipy="python -c 'import IPython; IPython.terminal.ipapp.launch_new_instance()'"
